67f433bf94aab46265fee4ef8be1f54ecc86943f
[fandong] <[fandong@qq.com]>
2019-07-07 15:34:30 +0800

    update

--- type_system/type_method.go
+++ type_system/type_method.go
@@ -32,3 +32,106 @@ p.name = "andes"
 p.age = 18
 // 使用构造函数进行初始化
 
+// 结构字段的特点
+
+//标准库container/list
+
+type Element struct {
+	// 指向自身类型的指针
+	next, prev *Element
+	list *List
+	Value interface()
+}
+
+// 自定义接口类型
+// interface{}是接口字面量类型标识符， 所以i是非命名类型变量
+// Reader 是自定义接口类型，属于命名类型
+type Reader interface{
+	Read(p []byte) (n int, err error)
+}
+
+// 类型方法
+// 类型方法接收者是值类型
+func (t TypeName)MethodName (ParaList) (Returnlist){
+	// method body
+}
+
+// 类型方法接收者是指针
+func (t *TypeName)MethodName(ParaList) (Returnlist)  {
+	// method body
+}
+
+// t是接收者， 可以自由指定名称
+// TypeName为命名类型的类型名
+// MethodName为方法名，是一个自定义标识符
+// ParamList是形参列表
+// ReturnList是返回值列表
+
+// 将类型的方法改写为常规函数
+// 类型方法接收者是值类型
+func TypeName_MethodName(t TypeName, otherParamList) (Returnlist) {
+	//method body
+}
+// 类型方法接收者是指针
+func TypeName_MethodName(t *TypeName, otherParamList) (Returnlist) {
+	//method body
+}
+
+// 示例
+type SliceInt []int
+func (s SliceInt) Sum() int {
+	sum := 0
+	for _, i := range s{
+		sum += i
+	}
+	return sum
+}
+
+//和上面方法等价
+func SliceInt_Sum(s SliceInt) int {
+	sum := 0
+	for _, i := range s {
+		sum += i
+	}
+	return sum
+}
+
+var s SliceInt = [] int{1,2,3,4}
+s.Sum()
+SliceInt_Sum(s)
+
+// 方法调用
+// 一般调用
+type T struct{
+	a int
+}
+func (t T) Get() int {
+	return t.a
+}
+
+func (t *T) Set(i int)  {
+	t.a = i
+}
+
+var t = &T{}
+// 普通方法调用
+t.Set(2)
+// 普通方法调用
+t.Get()
+
+// 方法值ß
+
+func (t *T) Print() {
+	fmt.Printf("%p, %v, %d\n", t, t, t.a)
+}
+
+// method value
+f := t.Set
+
+//方法值调用
+f(2)
+t.Print()		// 2
+
+//方法值调用
+f(3)
+t.Print()		// 3
